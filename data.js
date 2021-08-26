ee36fad0d3cd399f0842d58cfe5704991305545f
Pierre Constantineau <jpconstantineau@gmail.com>
2021-08-25 18:16:47 -0600

    trying scripted

---
+++ data.js
@@ -0,0 +1,14 @@
+myFunction([
+    {
+    "display": "JavaScript Tutorial",
+    "status": "checked"
+    },
+    {
+    "display": "HTML Tutorial",
+    "status": ""
+    },
+    {
+    "display": "CSS Tutorial",
+    "status": ""
+    }
+    ]);
